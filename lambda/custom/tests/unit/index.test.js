781db36851e0300eba978fe3344fe7e7c820858c
hideokamoto <hide.okamoto@digitalcube.jp>
2018-07-01 19:37:55 +0900

    ask utils

--- lambda/custom/tests/unit/index.test.js
+++ lambda/custom/tests/unit/index.test.js
@@ -8,26 +8,7 @@ const { handler , HelpIntentHandler, GetAddressIntent} = require('../../index.js
 
 const { ResponseFactory, AttributesManagerFactory } = require('ask-sdk-core')
 const { ServiceClientFactory, apiClient } = services
-
-const getHandlerInput = (requestEnvelope, context = {}) => {
-  const handlerInput = {
-    requestEnvelope,
-    context,
-    attributesManager: AttributesManagerFactory.init({
-      requestEnvelope,
-      persistenceAdapter: PersistenceAdapter
-    }),
-    responseBuilder: ResponseFactory.init(),
-    serviceClientFactory: apiClient
-      ? new ServiceClientFactory({
-        apiClient: apiClient,
-        apiEndpoint: requestEnvelope.context.System.apiEndpoint,
-        authorizationValue: requestEnvelope.context.System.apiAccessToken
-      })
-      : undefined
-  }
-  return handlerInput
-}
+const { getHandlerInput } = require('ask-utils')
 
 const event = {
   session: {
